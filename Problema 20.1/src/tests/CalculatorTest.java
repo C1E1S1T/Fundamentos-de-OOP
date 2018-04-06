2c842613a15c4d8981822e48587699cdacec5060
LAPTOP <LAPTOP@DESKTOP-VVGK202>
2018-04-05 19:23:24 -0800

    Feat-Calculator add,multiply,subtract,add

---
+++ Problema 20.1/src/tests/CalculatorTest.java
@@ -0,0 +1,49 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import models.Calculator;
+
+class CalculatorTest 
+{
+
+	@Test
+	void testAdd() 
+	{
+		Calculator calculator = new Calculator();
+		Double actual = calculator.add(3.0,4.0);
+		Double expected = 7.0;
+		assertEquals(expected,actual);
+	}
+	
+	@Test
+	void testSubstract() 
+	{
+		Calculator calculator = new Calculator();
+		Double actual = calculator.substract(4.0,5.0);
+		Double expected = -1.0;
+		assertEquals(expected,actual);
+	}
+	
+	@Test
+	void testDivide() 
+	{
+		Calculator calculator = new Calculator();
+		Double actual = calculator.divide(10.0,5.0);
+		Double expected = 2.0;
+		assertEquals(expected,actual);
+	}
+	
+	@Test
+	void testMultiply() 
+	{
+		Calculator calculator = new Calculator();
+		Double actual = calculator.multiply(10.0,5.0);
+		Double expected = 50.0;
+		assertEquals(expected,actual);
+	}
+	
+
+}
