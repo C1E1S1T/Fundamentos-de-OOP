ba07a1acff0aaaeb15cffcae1ba4a23bf29c866c
LAPTOP <LAPTOP@DESKTOP-VVGK202>
2018-04-07 09:28:35 -0800

    Feat - background music

--- Problema 20.3/src/app/Aplication.java
+++ Problema 20.3/src/app/Aplication.java
@@ -3,14 +3,14 @@ package app;
 public class Aplication extends UIAplication
 {
 
-	public Aplication(String nameApp) 
+	public Aplication(String nameApp, String resourcesPath) 
 	{
-		super(nameApp);
+		super(nameApp, resourcesPath);
 	}
 
 	public static void main(String[] args) 
 	{
-		new Aplication("Aprendiendo Swing").start();
+		new Aplication("Aprendiendo Swing","resources").start();
 	}
 
 	
--- Problema 20.3/src/app/UIAplication.java
+++ Problema 20.3/src/app/UIAplication.java
@@ -2,17 +2,24 @@ package app;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.io.File;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+
+import com.raysmond.player.BasicPlayer;
+import com.raysmond.player.BasicPlayerException;
 
 public abstract class UIAplication 
 {
 	private JFrame frame;
+	private String resourcesPath;
 	
-	public UIAplication(String nameApp)
+	public UIAplication(String nameApp, String resourcesPath)
 	{
 		frame = new JFrame(nameApp+" - CEST");
 		designFrame();
+		this.resourcesPath = resourcesPath;
 	}
 	
 	private void designFrame()
@@ -20,7 +27,7 @@ public abstract class UIAplication
 		frame.setSize(calculateWindowSize());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
-		frame.setIconImage(new ImageIcon("resources\\icon.png").getImage());
+		frame.setIconImage(new ImageIcon(resourcesPath, "icon.png").getImage());
 	}
 	
 	private Dimension calculateWindowSize()
@@ -32,11 +39,29 @@ public abstract class UIAplication
 		return windowSize;
 	}
 	
+	private void playBackgroundMusic(String name, String audioFormat)
+	{
+		BasicPlayer player = new BasicPlayer();
+		String soundName = name+"."+audioFormat;
+		try 
+		{
+			player.open(new File(resourcesPath,soundName));
+		} catch (BasicPlayerException e) 
+		{
+			e.printStackTrace();
+		}
+		try 
+		{
+			player.play();
+		} catch (BasicPlayerException e) 
+		{
+			e.printStackTrace();
+		}
+	}
 
-	
 	public void start()
 	{
-		
+		playBackgroundMusic("Background2","mp3");
 		frame.setVisible(true);
 	}
 	
